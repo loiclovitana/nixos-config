830e1e2210586f58fa3848430ed8e68bfbe30cfc
Loïc Vandenberghe <loiclovitana@gmail.com>
2026-09-03 18:28:15 +0200

    hotfix: stop fprintd on shutdown

--- modules/fprintd.nix
+++ modules/fprintd.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.fprintd.enable = true;
+
+  # hyprlock leaks the reader's claim when suspending mid-verify
+  # (hyprwm/hyprlock#768), wedging fprintd until reboot.
+  powerManagement.powerDownCommands = "${pkgs.systemd}/bin/systemctl stop fprintd.service";
+  systemd.services.fprintd.serviceConfig.TimeoutStopSec = "5s";
 }
